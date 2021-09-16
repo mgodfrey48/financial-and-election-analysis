baf1bc811ca8ee865b0887b1a8c508df44fed5b0
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-09-15 23:42:45 -0500

    Created another dictionary to store the percentages with the same keys as the vote counts dictionary (added a formatting command found from stack overflow). Searched through vote counts dictionary to find the election winner. Printed the results in terminal.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,33 +17,8 @@ with open(election_file_path, 'r') as csvfile:
     csv_header = next(csvreader)
 
     # create a dictionary to store candidate info, and a variable to store the vote count
-    candidates = {}
+    candidate_vote_counts = {}
     total_votes = 0
-
-    # set the first key in the candidates dictionary
-    first_vote = next(csvreader)
-    candidates[first_vote[2]] = 1
-    
-#TEST CODE ---------------------------------------
-
-    # first_vote = next(csvreader)
-    # print(first_vote)
-
-    # candidates[first_vote[2]] = 1
-    # print(candidates)
-
-    # next_vote = next(csvreader)
-    # print(next_vote)
-
-    # candidates[next_vote[2]] = 1
-    # print(candidates)
-
-    # for row in csvreader:
-    #     candidates[row[2]] += 1
-    #     print(candidates)
-
-
-#END TEST CODE -----------------------------------
 
     # read through each row of the data
     for row in csvreader:
@@ -55,33 +30,38 @@ with open(election_file_path, 'r') as csvfile:
         total_votes += 1
 
         # check to see if the candidate voted for is already in the dictionary
-        if voted_for in candidates:
-            candidates[voted_for] += 1
-        else:
-            candidates[voted_for] = 1
-
         # if so, add 1 to the count for that candidate
-            # candidates
+        if voted_for in candidate_vote_counts:
+            candidate_vote_counts[voted_for] += 1
+        
         # if not, add the new candidate to the dictionary and set there count equal to 1 
-        # for key in candidates:
+        else:
+            candidate_vote_counts[voted_for] = 1
+    
+    # create a dictionary to store the percentages of votes each candidate received
+    candidate_vote_percentages = {}
 
-        #     # If the vote matches a candidate, add 1 to the count of that candidate (the value associated to their key) 
-        #     if voted_for == key:
-        #         candidates[key] += 1
-           
-        #     # If the candidate is not found, add their name as a key to the dictionary of candidates,
-        #     # set/create their count variable to 1 
-        #     else:
-        #         candidates[voted_for] = 1
+    # calculate percentages of votes each candidate got, store results in voting percentage dictionary
+    for key in candidate_vote_counts:
+        candidate_vote_percentages[key] = '{:.3%}'.format(candidate_vote_counts[key]/total_votes) #this format function was found using stack overflow
 
-    print(candidates)
+    # define values to determine and store the winner of the election
+    winning_vote_count = 0
+    election_winner = ''
 
-            
-
-        # next row
-
-    # calculate percentages of votes each candidate got, store results
-
-    # sort the candidates based on percentage of votes received, most to least
-
+    # determine the highest vote count, and store the key associated with that count to the election winner variable
+    for key, value in candidate_vote_counts.items():
+        if value > winning_vote_count:
+            winning_vote_count = value
+            election_winner = key
+    
     # print results
+    print('Election Results')
+    print('-------------------------------')
+    print(f'Total Votes: {total_votes}')
+    print('-------------------------------')
+    for key in candidate_vote_percentages:
+        print(f'{key}: {candidate_vote_percentages[key]} ({candidate_vote_counts[key]})')
+    print('-------------------------------')
+    print(f'Winner: {election_winner}')
+    print('-------------------------------')
