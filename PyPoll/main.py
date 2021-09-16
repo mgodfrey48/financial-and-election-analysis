e2ccff13642cc264b0777555e30188f541601f4e
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-09-15 22:23:56 -0500

    Added some pseudocode to develop my algorithm, wrote some test code to try adding candidates to the dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,9 +7,6 @@ import csv
 # create file path for csv file
 election_file_path = os.path.join('Resources', 'election_data.csv')
 
-# create variables and/or dictionaries
-candidates = {}
-
 # open up the election data csv file
 with open(election_file_path, 'r') as csvfile:
 
@@ -18,17 +15,60 @@ with open(election_file_path, 'r') as csvfile:
 
     # store the header row
     csv_header = next(csvreader)
-    
-    # read through each row of the data
-    for row in csvreader:
-        print(row)
 
-        # check the name of the candidate: if new, add their name to the dictionary of candidates
-        # crete their count variable and add to their count. If not new, add one to the candidate's count.
+    # create a dictionary to store candidate info, and a variable to store the vote count
+    candidates = {}
+    total_votes = 0
+    
+#TEST CODE ---------------------------------------
+
+    first_vote = next(csvreader)
+    print(first_vote)
+
+    candidates[first_vote[2]] = 1
+    print(candidates)
+
+    next_vote = next(csvreader)
+    print(next_vote)
+
+    candidates[next_vote[2]] = 1
+    print(candidates)
+
+
+#END TEST CODE -----------------------------------
+
+
+
+
+    # read through each row of the data
+    # for row in csvreader:
+
+        # store the candidate voted for
+        # voted_for = row[2]
+        # print(voted_for)
+
+        # if no votes have been counted yet (aka the candidate dictionary is empty), create
+        # the first entry in the dictionary
+        # if total_votes == 0:
+        #     candidates = {row[2] : 1}
+
+        # # add to total vote count
+        # total_votes += 1
+
+        # search the candidate dictionary for the vote recipient   
+        # for key in candidates:
+
+            # If the vote matches a candidate, add 1 to the count of that candidate (the value associated to their key) 
+            # if row[2] == key:
+            #     candidates[key] += 1
+            # else:
+            #     candidates[row[2]]
+
+            # If the candidate is not found, add their name as a key to the dictionary of candidates,
+            # set/create their count variable to store as the key and add to the count 
+
 
         # next row
-
-    # total up all of the votes
 
     # calculate percentages of votes each candidate got, store results
 
