55c7911da3a250800b88eca77e36c2cb14010260
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-09-15 22:46:42 -0500

    Added/edited the pqeudocode. Added the if/else statement to create the dictionary of candidates as keys with their vote counts as the values.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,54 +19,64 @@ with open(election_file_path, 'r') as csvfile:
     # create a dictionary to store candidate info, and a variable to store the vote count
     candidates = {}
     total_votes = 0
+
+    # set the first key in the candidates dictionary
+    first_vote = next(csvreader)
+    candidates[first_vote[2]] = 1
     
 #TEST CODE ---------------------------------------
 
-    first_vote = next(csvreader)
-    print(first_vote)
+    # first_vote = next(csvreader)
+    # print(first_vote)
 
-    candidates[first_vote[2]] = 1
-    print(candidates)
+    # candidates[first_vote[2]] = 1
+    # print(candidates)
 
-    next_vote = next(csvreader)
-    print(next_vote)
+    # next_vote = next(csvreader)
+    # print(next_vote)
 
-    candidates[next_vote[2]] = 1
-    print(candidates)
+    # candidates[next_vote[2]] = 1
+    # print(candidates)
+
+    # for row in csvreader:
+    #     candidates[row[2]] += 1
+    #     print(candidates)
 
 
 #END TEST CODE -----------------------------------
 
-
-
-
     # read through each row of the data
-    # for row in csvreader:
+    for row in csvreader:
 
         # store the candidate voted for
-        # voted_for = row[2]
-        # print(voted_for)
+        voted_for = row[2]
 
-        # if no votes have been counted yet (aka the candidate dictionary is empty), create
-        # the first entry in the dictionary
-        # if total_votes == 0:
-        #     candidates = {row[2] : 1}
+        # add to total vote count
+        total_votes += 1
 
-        # # add to total vote count
-        # total_votes += 1
+        # check to see if the candidate voted for is already in the dictionary
+        if voted_for in candidates:
+            candidates[voted_for] += 1
+        else:
+            candidates[voted_for] = 1
 
-        # search the candidate dictionary for the vote recipient   
+        # if so, add 1 to the count for that candidate
+            # candidates
+        # if not, add the new candidate to the dictionary and set there count equal to 1 
         # for key in candidates:
 
-            # If the vote matches a candidate, add 1 to the count of that candidate (the value associated to their key) 
-            # if row[2] == key:
-            #     candidates[key] += 1
-            # else:
-            #     candidates[row[2]]
+        #     # If the vote matches a candidate, add 1 to the count of that candidate (the value associated to their key) 
+        #     if voted_for == key:
+        #         candidates[key] += 1
+           
+        #     # If the candidate is not found, add their name as a key to the dictionary of candidates,
+        #     # set/create their count variable to 1 
+        #     else:
+        #         candidates[voted_for] = 1
 
-            # If the candidate is not found, add their name as a key to the dictionary of candidates,
-            # set/create their count variable to store as the key and add to the count 
+    print(candidates)
 
+            
 
         # next row
 
